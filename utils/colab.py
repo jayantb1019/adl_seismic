7682f7bc2c20387ac5e42610bdcb41d97e35003e
jayanthboddu <jayantb1019@hotmail.com>
2023-02-17 12:12:07 +0530

    colab_utils and training

--- utils/colab.py
+++ utils/colab.py
@@ -4,7 +4,7 @@ from google.colab import drive
 drive.mount('/content/drive')
 
 !pip install -qq rich pytorch-lightning==1.7.7 scikit-image==0.19.2
-ne https://jayantb1019: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@github.com/jayantb1019/adl_seismic.git
+!git clone https://jayantb1019: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@github.com/jayantb1019/adl_seismic.git
 !mkdir -p /content/adl_seismic/data
 !unzip /content/drive/MyDrive/faciesmark.zip -d /content/adl_seismic/data/.
 !cp /content/drive/MyDrive/denoiser_20230213_epoch=49-step=27600.ckpt /content/denoiser_20230213_epoch=49-step=27600.ckpt
