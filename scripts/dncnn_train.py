52a7f22227f27042d35b752ba5112c8e752cbefc
jayb <jayantb1019@hotmail.com>
2023-03-02 13:48:15 +0530

    dncnn training argparse added

--- scripts/dncnn_train.py
+++ scripts/dncnn_train.py
@@ -19,7 +19,7 @@ from rich import traceback
 
 import pytorch_lightning as pl 
 from pytorch_lightning.callbacks.progress import TQDMProgressBar
-from pytorch_lightning.callbacks import RichModelSummary, ModelCheckpoint,StochasticWeightAveraging
+from pytorch_lightning.callbacks import RichModelSummary, ModelCheckpoint,EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger
 
 import sys 
@@ -55,19 +55,41 @@ def main(args) :
         
     if args['e'] : # epochs , if provided through cmdline
         config['train']['epochs'] = args['e']
-        
-        
+
+    if args['nt'] : 
+        config['train']['data']['noise_type'] = args['nt']
+    
+    if args['nl'] : 
+        config['train']['data']['noise_factor'] = args['nl']
+
+    if args['nlayers'] : 
+        config['train']['dncnn']['nlayers'] = args['nlayers']
+
+    if args['act'] :
+        config['train']['dncnn']['act'] = args['act']
+
+    if args['w'] :
+        config['train']['dncnn']['w'] = args['w']  
+
+    if args['bias'] != None :
+        config['train']['dncnn']['bias'] = args['bias'] 
+
+    
+
     limit_train_batches = args['lt']
     limit_val_batches = args['lv']
 
     modelSummaryCb = RichModelSummary(max_depth=-1)
     tqdmProgressCb = TQDMProgressBar(refresh_rate=20)
     modelCheckpointCb = ModelCheckpoint(save_top_k = 5, monitor = 'val_ssim', mode='max')
+    earlyStoppingCb = EarlyStopping(monitor='val_ssim',mode='max', patience=10)
     
     
     # logger 
     timestamp = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
-    experiment_version = f"dncnn_{timestamp}"
+    noise_mode = config['train']['data']['noise_type'] 
+    noise_level = config['train']['data']['noise_factor'] 
+    experiment_version = f"dncnn_epochs_{config['train']['epochs']}_lt_{limit_train_batches}_{noise_mode}_{noise_level}_{timestamp}"
     dncnn_logger = TensorBoardLogger('../lightning_logs', name='dncnn', log_graph=True, version = experiment_version)
     
     datamodule = FaciesMarkDataModule(config['train']['data'])
@@ -76,7 +98,7 @@ def main(args) :
     dncnn_trainer = pl.Trainer(
         accelerator = accelerator,
         devices=1, 
-        callbacks = [modelSummaryCb, tqdmProgressCb , modelCheckpointCb, StochasticWeightAveraging(swa_lrs=1e-2) ],
+        callbacks = [modelSummaryCb, tqdmProgressCb , modelCheckpointCb, earlyStoppingCb ],
         logger = dncnn_logger,
         max_epochs=config['train']['epochs'], 
         fast_dev_run=fast_dev_run,          
@@ -84,7 +106,7 @@ def main(args) :
         limit_train_batches = limit_train_batches, 
         limit_val_batches = limit_val_batches, 
         log_every_n_steps = 5,
-        precision=16,
+        # precision=16,
         track_grad_norm=2, # this will plot norm-2 to tensorboard, if its increasing, then the gradients would explode.
         detect_anomaly = True, # detects nans in forward / backward pass and stops training
         gradient_clip_val=0.5
@@ -103,6 +125,12 @@ if __name__ == '__main__' :
     parser.add_argument('-lt', type=float, default = 0.99) # limit train batches
     parser.add_argument('-lv', type=float, default = 0.99) # limit val batches
     parser.add_argument('-e', type=int, default=None)
+    parser.add_argument('-nt', type=str, default='gaussian') # noise type, could be gaussian, poisson, mixed, lpf
+    parser.add_argument('-nl', type=float, default = 0.5) # noise level, between 0-1. Prefer 0.1,0.5,0.9 # only works for gaussian
+    parser.add_argument('-nlayers', type=int, default = 15) # no of layers 
+    parser.add_argument('-w', type=int, default=64) # width / no of filters 
+    parser.add_argument('-act', type=str, default='IL') # can be BL = batch norm + leaky relu or IL = Instance Norm + leaky relu
+    parser.add_argument('-bias', type=bool, default=False)
 
     
     bs = parser.parse_args().bs
@@ -111,6 +139,23 @@ if __name__ == '__main__' :
     lt = parser.parse_args().lt
     lv = parser.parse_args().lv
     e = parser.parse_args().e
+    nt = parser.parse_args().nt
+    nl = parser.parse_args().nl
+    nlayers = parser.parse_args().nlayers
+    act = parser.parse_args().act
+    w = parser.parse_args().w
+    bias = parser.parse_args().bias
 
-    args = dict(bs = bs , accelerator = accelerator, loc=loc , lt = lt, lv = lv, e = e)
+    args = dict(bs = bs , 
+                accelerator = accelerator, 
+                loc=loc , 
+                lt = lt, 
+                lv = lv,
+                e = e, 
+                nt=nt, 
+                nl=nl, 
+                nlayers = nlayers, 
+                w = w, 
+                act = act, 
+                bias = bias)
     main(args)
