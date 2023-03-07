15417b3375974cab63c64dc2c93902a78a205581
jayanthboddu <jayantb1019@hotmail.com>
2023-03-07 21:06:54 +0530

    colab adl trained updated

--- scripts/adl_train_colab.py
+++ scripts/adl_train_colab.py
@@ -57,7 +57,7 @@ def main(args) :
     traceback.install()
     
     if args['loc'] == 'colab' : 
-        CONFIG_PATH = '/content/adl_seismic/config/config_adl_faciesmark_colab.yaml'
+        CONFIG_PATH = '/content/adl_seismic/config/config_adl_faciesmark_colab_final.yaml'
     elif args['loc'] == 'workstation' : 
         CONFIG_PATH = '/local1/workspace/adl_seismic/config/config_adl_faciesmark.yaml'
     
@@ -83,6 +83,33 @@ def main(args) :
         
     if args['lambda1'] : 
         config['train']['ADL']['lambda1'] = args['lambda1']
+        
+    if args['nt'] : 
+        config['train']['data']['noise_mode'] = args['nt']
+    
+    if args['nl'] : 
+        config['train']['data']['noise_factor'] = args['nl']
+        
+    if args['aug'] : 
+        config['train']['data']['augmentations'] = args['aug']
+        
+    
+    train_denoiser = False
+    train_discriminator = False
+    train_adl = False 
+    run_tests = False
+    
+    if args['denoiser'] == True : 
+        train_denoiser = True 
+    if args['discriminator'] == True : 
+        train_discriminator = True 
+    
+    if args['adl'] == True : 
+        train_adl = True 
+        
+    if args['tests'] == True : 
+        run_tests = True
+    
 
 
     limit_train_batches = args['lt']
@@ -94,7 +121,7 @@ def main(args) :
     
     # logger 
     timestamp = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
-    experiment_version = f"adl_{timestamp}"
+    experiment_version = f"adl_final_{timestamp}"
     denoiser_logger = TensorBoardLogger('../lightning_logs', name='denoiser', log_graph=True, version = experiment_version)
     discriminator_logger = TensorBoardLogger('../lightning_logs', name='discriminator', log_graph=True, version = experiment_version)
     adl_logger = TensorBoardLogger('../lightning_logs', name='adl', log_graph=True, version = experiment_version)
@@ -124,15 +151,6 @@ def main(args) :
 
     # denoiser_trainer.test(trained_denoiser, datamodule)
     # pdb.set_trace()
-
-    #PHASE 1 : 
-    print('''
-          ================
-          DENOISER WARM UP
-          ================
-          ''')
-    
-    denoiser = Efficient_U(config)
     
     if args['loc'] == 'kaggle' : 
         denoiser_checkpoint_path = '/kaggle/working/adl_seismic/checkpoints/denoiser.ckpt' # colab
@@ -140,114 +158,152 @@ def main(args) :
         denoiser_checkpoint_path = '/local1/workspace/adl_seismic/adl_seismic/lightning_logs/denoiser/adl_21_02_2023_15_09_14_best/checkpoints/epoch=49-step=27300.ckpt' # colab
     if args['loc'] == 'colab' : 
         denoiser_checkpoint_path = '/content/drive/MyDrive/adl_seismic/lightning_logs/denoiser/adl_21_02_2023_15_09_14_best/checkpoints/epoch=49-step=27300.ckpt'
+
+    #PHASE 1 : 
+    if train_denoiser : 
+        print('''
+            ================
+            DENOISER WARM UP
+            ================
+            ''')
         
-    # denoiser_checkpoint_path = '/local1/workspace/adl_seismic/lightning_logs/denoiser/adl_27_02_2023_18_16_16/checkpoints/epoch=59-step=27360.ckpt' # workstation
-    # print(f"Denoiser Checkpoint : {denoiser_checkpoint_path}")
-    # trained_denoiser = Efficient_U(config).load_from_checkpoint(denoiser_checkpoint_path)
-    denoiser_trainer = pl.Trainer(
-        accelerator = accelerator,
-        devices=1, 
-        callbacks = [modelSummaryCb, tqdmProgressCb , modelCheckpointCb, StochasticWeightAveraging(swa_lrs=1e-2) ],
-        logger = denoiser_logger,
-        max_epochs=config['train']['denoiser']['epochs'], 
-        fast_dev_run=fast_dev_run,          
-        enable_model_summary=False,
-        limit_train_batches = limit_train_batches, 
-        limit_val_batches = limit_val_batches, 
-        log_every_n_steps = 5,
-        # precision=32
-        track_grad_norm=2, # this will plot norm-2 to tensorboard, if its increasing, then the gradients would explode.
-        detect_anomaly = True, # detects nans in forward / backward pass and stops training
-        gradient_clip_val=0.5
-    )
+        denoiser = Efficient_U(config)
+        
+        
+            
+        # denoiser_checkpoint_path = '/local1/workspace/adl_seismic/lightning_logs/denoiser/adl_27_02_2023_18_16_16/checkpoints/epoch=59-step=27360.ckpt' # workstation
+        # print(f"Denoiser Checkpoint : {denoiser_checkpoint_path}")
+        # trained_denoiser = Efficient_U(config).load_from_checkpoint(denoiser_checkpoint_path)
+        
+        cbs = [modelSummaryCb, tqdmProgressCb , modelCheckpointCb ]
+        
+        if args['swa'] : 
+            cbs.append(StochasticWeightAveraging(swa_lrs=1e-2))
+            
+        denoiser_trainer = pl.Trainer(
+            accelerator = accelerator,
+            devices=1, 
+            callbacks = cbs,
+            logger = denoiser_logger,
+            max_epochs=config['train']['denoiser']['epochs'], 
+            fast_dev_run=fast_dev_run,          
+            enable_model_summary=False,
+            limit_train_batches = limit_train_batches, 
+            limit_val_batches = limit_val_batches, 
+            log_every_n_steps = 5,
+            # precision=32
+            track_grad_norm=2, # this will plot norm-2 to tensorboard, if its increasing, then the gradients would explode.
+            detect_anomaly = True, # detects nans in forward / backward pass and stops training
+            gradient_clip_val=0.5
+        )
+        
+        
+        denoiser_trainer.fit(denoiser, datamodule)
     
-    
-    denoiser_trainer.fit(denoiser, datamodule)
-    # denoiser_trainer.test(denoiser, datamodule)
-    # denoiser_trainer.test(trained_denoiser, datamodule)
+        if run_tests : 
+            denoiser_trainer.test(denoiser, datamodule)
+            
+    else : 
+        trained_denoiser = Efficient_U(config).load_from_checkpoint(denoiser_checkpoint_path)
+        if run_tests : 
+            denoiser_trainer.test(trained_denoiser, datamodule)
 
-    # pdb.set_trace()
-
-    # print(results)
-    
-    
     
     # PHASE 2 : 
-    # print('''
-    #       =====================
-    #       DISCRIMINATOR WARM UP
-    #       =====================
-    #       ''')
-
-    discriminator_trainer = pl.Trainer(
-        accelerator = accelerator,
-        devices=1, 
-        callbacks = [modelSummaryCb, tqdmProgressCb ],
-        logger = discriminator_logger,
-        max_epochs=config['train']['discriminator']['epochs'], 
-        fast_dev_run=fast_dev_run, 
-         enable_model_summary=False,      
-        limit_train_batches = limit_train_batches, 
-        limit_val_batches = limit_val_batches,
-        log_every_n_steps= 5, 
-        # overfit_batches= 10,
-        # check_val_every_n_epoch = 5
-         # precision=32   
-        track_grad_norm=2, # this will plot norm-2 to tensorboard, if its increasing, then the gradients would explode.
-        detect_anomaly = True, # detects nans in forward / backward pass and stops training
-        gradient_clip_val=0.5
-    )
     
+    if train_discriminator : 
+        # print('''
+        #       =====================
+        #       DISCRIMINATOR WARM UP
+        #       =====================
+        #       ''')
+
+        discriminator_trainer = pl.Trainer(
+            accelerator = accelerator,
+            devices=1, 
+            callbacks = [modelSummaryCb, tqdmProgressCb ],
+            logger = discriminator_logger,
+            max_epochs=config['train']['discriminator']['epochs'], 
+            fast_dev_run=fast_dev_run, 
+            enable_model_summary=False,      
+            limit_train_batches = limit_train_batches, 
+            limit_val_batches = limit_val_batches,
+            log_every_n_steps= 5, 
+            # overfit_batches= 10,
+            # check_val_every_n_epoch = 5
+            # precision=32   
+            track_grad_norm=2, # this will plot norm-2 to tensorboard, if its increasing, then the gradients would explode.
+            detect_anomaly = True, # detects nans in forward / backward pass and stops training
+            gradient_clip_val=0.5
+        )
+        
     # denoiser_checkpoint_path = '/local1/workspace/adl_seismic/lightning_logs/denoiser/adl_20_02_2023_12_50_02/checkpoints/epoch=49-step=2750.ckpt'
     
     # denoiser_checkpoint_path = '/local1/workspace/adl_seismic/lightning_logs/denoiser/adl_27_02_2023_14_32_03/checkpoints/epoch=49-step=250.ckpt'
     
-    # denoiser_checkpoint_path = get_checkpoint('denoiser',experiment_version, config)
-    trained_denoiser = Efficient_U(config).load_from_checkpoint(denoiser_checkpoint_path)
+        if train_denoiser : 
+            denoiser_checkpoint_path = get_checkpoint('denoiser',experiment_version, config)
+            
+        trained_denoiser = Efficient_U(config).load_from_checkpoint(denoiser_checkpoint_path)
     
     # pdb.set_trace()
     
     
-    discriminator = Efficient_U_DISC(trained_denoiser, config)
+        discriminator = Efficient_U_DISC(trained_denoiser, config)
+
+        discriminator_trainer.fit(discriminator, datamodule) 
+        
+        if run_tests : 
+            print('denoiser tests')
+            denoiser_trainer.test(trained_denoiser, datamodule)
+            print('discriminator tests')
+            discriminator_trainer.test(discriminator, datamodule)
     
-    
-    # discriminator_trainer.fit(discriminator, datamodule) 
-    # denoiser_trainer.test(trained_denoiser, datamodule)
-    
-    # denoiser_trainer.test(trained_denoiser, datamodule)
-    
-    # pdb.set_trace()
-    
+    else : 
+        if train_denoiser : 
+            denoiser_checkpoint_path = get_checkpoint('denoiser',experiment_version, config)
+            
+        trained_denoiser = Efficient_U(config).load_from_checkpoint(denoiser_checkpoint_path)
+        
+        if train_discriminator : 
+            discrminator_checkpoint_path = get_checkpoint('discriminator', experiment_version, config)
+            
+        trained_discriminator = Efficient_U_DISC.load_from_checkpoint(checkpoint_path = discrminator_checkpoint_path, 
+                                                                      denoiser=trained_denoiser, 
+                                                                      config=config)
+        if run_tests : 
+            denoiser_trainer.test(trained_denoiser, datamodule)
     # PHASE 3 : 
-    print('''
-          =====================
-              ADL TRAINING
-          =====================
-          ''')
-    
-    adl_trainer = pl.Trainer(
-        accelerator = accelerator,
-        devices=1, 
-        callbacks = [modelSummaryCb, tqdmProgressCb, modelCheckpointCb  ],
-        logger = adl_logger,
-        max_epochs=config['train']['ADL']['epochs'], 
-        fast_dev_run=fast_dev_run, 
-        enable_model_summary=False,       
-        limit_train_batches = limit_train_batches, 
-        limit_val_batches = limit_val_batches,
-        log_every_n_steps = 5,   
-        # overfit_batches=10,
-        track_grad_norm=2, # this will plot norm-2 to tensorboard, if its increasing, then the gradients would explode.
-        detect_anomaly = True, # detects nans in forward / backward pass and stops training
-        gradient_clip_val=0.5
-        # check_val_every_n_epoch = 5
-        # precision=32 
-    )
+    if train_adl : 
+        print('''
+            =====================
+                ADL TRAINING
+            =====================
+            ''')
+        
+        adl_trainer = pl.Trainer(
+            accelerator = accelerator,
+            devices=1, 
+            callbacks = [modelSummaryCb, tqdmProgressCb, modelCheckpointCb  ],
+            logger = adl_logger,
+            max_epochs=config['train']['ADL']['epochs'], 
+            fast_dev_run=fast_dev_run, 
+            enable_model_summary=False,       
+            limit_train_batches = limit_train_batches, 
+            limit_val_batches = limit_val_batches,
+            log_every_n_steps = 5,   
+            # overfit_batches=10,
+            track_grad_norm=2, # this will plot norm-2 to tensorboard, if its increasing, then the gradients would explode.
+            detect_anomaly = True, # detects nans in forward / backward pass and stops training
+            gradient_clip_val=0.5
+            # check_val_every_n_epoch = 5
+            # precision=32 
+        )
     
     
     
     # denoiser_checkpoint_path = get_checkpoint('denoiser', experiment_version, config)
-    discriminator_checkpoint_path = get_checkpoint('discriminator', experiment_version, config)
+        discriminator_checkpoint_path = get_checkpoint('discriminator', experiment_version, config)
     
     # pdb.set_trace()
     
@@ -257,15 +313,17 @@ def main(args) :
     # pdb.set_trace()
     
     # trained_denoiser = Efficient_U.load_from_checkpoint(checkpoint_path = denoiser_checkpoint_path, config = config)
-    trained_discriminator = Efficient_U_DISC.load_from_checkpoint(checkpoint_path = discriminator_checkpoint_path, model=trained_denoiser, config=config) 
+        trained_discriminator = Efficient_U_DISC.load_from_checkpoint(checkpoint_path = discriminator_checkpoint_path, model=trained_denoiser, config=config) 
     # trained_discriminator = Efficient_U_DISC.(model=trained_denoiser, config=config)
 
-    adl = ADL(trained_denoiser, trained_discriminator, config)
+        adl = ADL(trained_denoiser, trained_discriminator, config)
     
-    # adl_trainer.fit(adl, datamodule) 
-    # adl_trainer.test(adl, datamodule) 
+        adl_trainer.fit(adl, datamodule) 
+        
+        if run_tests : 
+            adl_trainer.test(adl, datamodule) 
     
-    # pdb.set_trace()
+    pdb.set_trace()
     
 
 if __name__ == '__main__' : 
@@ -278,6 +336,14 @@ if __name__ == '__main__' :
     parser.add_argument('-lv', type=float, default = 0.99) # limit val batches
     parser.add_argument('-e', type=int, default=None)
     parser.add_argument('-lambda1', type=int, default = None )
+    parser.add_argument('-nt', type=str, default='gaussian') # noise type, could be gaussian, poisson, mixed, lpf
+    parser.add_argument('-nl', type=float, default = 0.5) # noise level, between 0-1. Prefer 0.1,0.5,0.9 # only works for gaussian
+    parser.add_argument("-denoiser", type=bool, default=True)
+    parser.add_argument('-discrminator', type = bool, default = True)
+    parser.add_argument('-adl', type=bool, default=True)
+    parser.add_argument('-tests', type=bool, default=True)
+    parser.add_argument('-aug', type = bool, default = False)
+    parser.add_argument('-swa', type = bool, default = False)
 
     
     bs = parser.parse_args().bs
@@ -287,6 +353,18 @@ if __name__ == '__main__' :
     lv = parser.parse_args().lv
     e = parser.parse_args().e
     lambda1 = parser.parse_args().lambda1
+    noise_type = parser.parse_args().nt
+    noise_level = parser.parse_args().nl
+
     
-    args = dict(bs = bs , accelerator = accelerator, loc=loc , lt = lt, lv = lv, e = e, lambda1= lambda1)
+    pargs = parser.parse_args()
+    
+    
+    args = dict(bs = bs , accelerator = accelerator, loc=loc , lt = lt, lv = lv, e = e, lambda1= lambda1, nt = noise_type, nl = noise_level, 
+                denoiser = pargs.denoiser, 
+                discriminator = pargs.discriminator,
+                adl = pargs.adl, 
+                tests = pargs.tests, 
+                aug = pargs.aug, 
+                swa = pargs.swa)
     main(args)
